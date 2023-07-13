ecccaa4367a78b01541d56a6e80486314df6fc79
Abhiraj Padhye <59491581+abcdefghiraj@users.noreply.github.com>
2023-07-13 16:04:52 +0200

    Add action functionality.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,83 @@
-import * as core from '@actions/core'
+import fs from 'fs'
+import path from 'path'
+import core from '@actions/core'
+import exec from '@actions/exec'
+import {execa, $} from 'execa'
 import {wait} from './wait'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+const setupScript = core.getInput('setup', {required: true})
+const preBuildScript = core.getInput('prebuild', {required: true})
+const buildScript = core.getInput('build', {required: true})
+const serveScript = core.getInput('serve', {required: true})
+const cleanupScript = core.getInput('cleanup', {required: true})
+const url = core.getInput('url', {required: true})
+const minScore = core.getInput('min_score', {required: true})
+const sleepTime = core.getInput('wait_time', {required: true})
+// TODO: Need a validator for scripts.
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+async function run() {
+  core.startGroup('Stark Accessibility Checker: Setup')
+  await exec.exec(setupScript)
+  core.endGroup()
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+  core.startGroup('Stark Accessibility Checker: Prebuild')
+  await exec.exec(preBuildScript)
+  core.endGroup()
+
+  core.startGroup('Stark Accessibility Checker: Build')
+  await exec.exec(buildScript)
+  core.endGroup()
+
+  core.startGroup('Stark Accessibility Checker: Serve & Scan')
+  core.info(`Sleeping for ${sleepTime} ms. Giving the start command time to complete!`)
+  // TODO: Pipe stdio to file stream.
+  const childProcess = $({
+    shell: true,
+    detached: true,
+    stdio: 'inherit'
+  })`${serveScript}`
+
+  
+  await wait(Number.parseInt(sleepTime))
+  // TODO: Also pipe to logs
+  await execa('slay', ['scan', '--url', url, '--min-score', minScore], {
+    stdio: 'inherit'
+  })
+
+  core.info('Shutting down server. Scanning done.')
+  childProcess.unref()
+
+  const cliOutDir = path.resolve(process.cwd(), './.stark-contrast/')
+  const files = fs.readdirSync(cliOutDir)
+  const results = []
+
+  for (const i in files) {
+    if (path.basename(files[i]) === 'summary.json') {
+      const filePath = path.resolve(cliOutDir, files[i])
+      const json = JSON.parse(fs.readFileSync(filePath, 'utf8'))
+      results.push(json)
+    }
   }
+
+  //TODO: Format better. Add error checking
+  const tableData = []
+  for (let data of results[0].data) {
+    tableData.push([data.name, data.value + ''])
+  }
+
+  await core.summary
+    .addHeading(`Accessibility results Summary`)
+    .addHeading(url, 4)
+    .addTable(tableData)
+    .addLink('View the full results', 'https://getstark.co') // TODO: Get link
+    .addSeparator()
+    .write()
+
+  core.endGroup()
+
+  core.startGroup('Stark Accessibility Checker: Cleanup')
+  await exec.exec(cleanupScript)
+  core.endGroup()
 }
 
 run()
