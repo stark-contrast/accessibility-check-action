9b6ed5983f79bc6bf2e21a212cc199bbd59fb0a1
Abhiraj Padhye <59491581+abcdefghiraj@users.noreply.github.com>
2023-07-25 19:35:11 +0200

    Add temporary build to test gh data

--- src/main.ts
+++ src/main.ts
@@ -1,89 +1,89 @@
-import * as path from 'path'
+// import * as path from 'path'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import * as exec from '@actions/exec'
-import {execa, $} from 'execa'
-import {wait} from './wait'
-import {readResults} from './read-results'
+// import * as exec from '@actions/exec'
+// import {execa, $} from 'execa'
+// import {wait} from './wait'
+// import {readResults} from './read-results'
 
-const setupScript = core.getInput('setup', {required: true})
-const preBuildScript = core.getInput('prebuild', {required: true})
-const buildScript = core.getInput('build', {required: true})
-const serveScript = core.getInput('serve', {required: true})
-const cleanupScript = core.getInput('cleanup', {required: true})
-const url = core.getInput('url', {required: true})
-const minScore = core.getInput('min_score', {required: true})
-const sleepTime = core.getInput('wait_time', {required: true})
-const token = core.getInput('token', {required: false})
+// const setupScript = core.getInput('setup', {required: true})
+// const preBuildScript = core.getInput('prebuild', {required: true})
+// const buildScript = core.getInput('build', {required: true})
+// const serveScript = core.getInput('serve', {required: true})
+// const cleanupScript = core.getInput('cleanup', {required: true})
+// const url = core.getInput('url', {required: true})
+// const minScore = core.getInput('min_score', {required: true})
+// const sleepTime = core.getInput('wait_time', {required: true})
+// const token = core.getInput('token', {required: false})
 // TODO: Need a validator for scripts.
 
-async function run(): Promise<void> {
-  core.startGroup('Stark Accessibility Checker: Setup')
-  await exec.exec(setupScript)
-  core.endGroup()
+// async function run(): Promise<void> {
+//   core.startGroup('Stark Accessibility Checker: Setup')
+//   await exec.exec(setupScript)
+//   core.endGroup()
 
-  core.startGroup('Stark Accessibility Checker: Prebuild')
-  await exec.exec(preBuildScript)
-  core.endGroup()
+//   core.startGroup('Stark Accessibility Checker: Prebuild')
+//   await exec.exec(preBuildScript)
+//   core.endGroup()
 
-  core.startGroup('Stark Accessibility Checker: Build')
-  await exec.exec(buildScript)
-  core.endGroup()
+//   core.startGroup('Stark Accessibility Checker: Build')
+//   await exec.exec(buildScript)
+//   core.endGroup()
 
-  core.startGroup('Stark Accessibility Checker: Serve & Scan')
-  core.info(
-    `Sleeping for ${sleepTime} ms. Giving the start command time to complete!`
-  )
-  // TODO: Pipe stdio to file stream.
-  const childProcess = $({
-    shell: true,
-    detached: true,
-    stdio: 'inherit'
-  })`${serveScript}`
+//   core.startGroup('Stark Accessibility Checker: Serve & Scan')
+//   core.info(
+//     `Sleeping for ${sleepTime} ms. Giving the start command time to complete!`
+//   )
+//   // TODO: Pipe stdio to file stream.
+//   const childProcess = $({
+//     shell: true,
+//     detached: true,
+//     stdio: 'inherit'
+//   })`${serveScript}`
 
-  await wait(Number.parseInt(sleepTime))
-  // TODO: Also pipe to logs
-  const params = ['scan', '--url', url, '--min-score', minScore]
-  if (token) {
-    // TODO: change this to be 2 separate things
-    params.push('--stark-token', token)
-    params.push('--run-id', token)
-  }
-  params.push('--metadata', JSON.stringify(github.context.workflow))
-  // TODO: Check run id
-  await execa('stark-accessibility', params, {
-    stdio: 'inherit'
-  })
+//   await wait(Number.parseInt(sleepTime))
+//   // TODO: Also pipe to logs
+//   const params = ['scan', '--url', url, '--min-score', minScore]
+//   if (token) {
+//     // TODO: change this to be 2 separate things
+//     params.push('--stark-token', token)
+//     params.push('--run-id', token)
+//   }
+//   params.push('--metadata', JSON.stringify(github.context.workflow))
+//   // TODO: Check run id
+//   await execa('stark-accessibility', params, {
+//     stdio: 'inherit'
+//   })
 
-  core.info('Shutting down server. Scanning done.')
-  childProcess.unref()
+//   core.info('Shutting down server. Scanning done.')
+//   childProcess.unref()
 
-  const cliOutDir = path.resolve(process.cwd(), './.stark-contrast/')
-  const results = await readResults(cliOutDir)
+//   const cliOutDir = path.resolve(process.cwd(), './.stark-contrast/')
+//   const results = await readResults(cliOutDir)
 
-  //TODO: Format better. Add error checking
-  const tableData = []
-  //TODO: Handling if results = []
-  for (const data of results[0].data) {
-    tableData.push([data.name, `${data.value}  `])
-  }
+//   //TODO: Format better. Add error checking
+//   const tableData = []
+//   //TODO: Handling if results = []
+//   for (const data of results[0].data) {
+//     tableData.push([data.name, `${data.value}  `])
+//   }
 
-  await core.summary
-    .addHeading(`Accessibility results Summary`)
-    .addHeading(url, 4)
-    .addTable(tableData)
-    .addLink('View the full results', 'https://getstark.co') // TODO: Get link
-    .addSeparator()
-    .write()
+//   await core.summary
+//     .addHeading(`Accessibility results Summary`)
+//     .addHeading(url, 4)
+//     .addTable(tableData)
+//     .addLink('View the full results', 'https://getstark.co') // TODO: Get link
+//     .addSeparator()
+//     .write()
 
-  core.endGroup()
+//   core.endGroup()
 
-  core.startGroup('Stark Accessibility Checker: Cleanup')
-  await exec.exec(cleanupScript)
-  core.endGroup()
-  return
-}
+//   core.startGroup('Stark Accessibility Checker: Cleanup')
+//   await exec.exec(cleanupScript)
+//   core.endGroup()
+//   return
+// }
 
 //run()
 
-console.log(JSON.stringify(github))
+core.info(JSON.stringify(github))
